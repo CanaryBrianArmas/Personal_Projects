9352d72050683dd88d89eeb8aa2a9a8badd6a652
Brian <157855560+CanaryBrianArmas@users.noreply.github.com>
2025-02-12 10:20:46 +0000

    Modifico los epochs a 1

--- Deep_Learning/NLP/Helsinki_Opus_Books/src/config.py
+++ Deep_Learning/NLP/Helsinki_Opus_Books/src/config.py
@@ -3,7 +3,7 @@
 MODEL_NAME = "Helsinki-NLP/opus-mt-en-es"
 DATASET_NAME = "Helsinki-NLP/opus_books"
 DATASET_CONFIG = "en-es"
-NUM_TRAIN_EPOCHS = 3
+NUM_TRAIN_EPOCHS = 1
 PER_DEVICE_TRAIN_BATCH_SIZE = 22
 
 # Language pair settings
@@ -15,7 +15,7 @@ TRAINING_ARGS = {
     "output_dir": "./results",
     "num_train_epochs": NUM_TRAIN_EPOCHS,
     "per_device_train_batch_size": PER_DEVICE_TRAIN_BATCH_SIZE,
-    "per_device_eval_batch_size": 8,
+    "per_device_eval_batch_size": 16,
     "evaluation_strategy": "epoch",
     "save_strategy": "epoch",
     "logging_dir": "./logs"
